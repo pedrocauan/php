b1c965e305c841b1a4127efd65cd500e7d615eb1
pedrocauan <mainyasuo92@gmail.com>
2022-06-24 21:24:25 -0300

    Criando barra de navegação

---
+++ revisao/exercicio.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--fonte-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anek+Latin:wght@100;200;300;400;500;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="exercicio.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso Php</h1>
+        <h2>Visualização do exercicio</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+           
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS &COPY; <?= date('Y'); ?>
+    </footer>
+</body>
+
+</html>
