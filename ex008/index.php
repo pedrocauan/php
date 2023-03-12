c657bd92a324c43e1ab02040aa0723725bde5259
pedrocauan <mainyasuo92@gmail.com>
2023-03-12 18:01:47 -0300

    array_merge, array_values, array_pop, array_shift

--- ex008/index.php
+++ ex008/index.php
@@ -3,12 +3,42 @@
      * is_array() -> ve se é um array
      * in_array() -> ve se o valor existe no array
      * array_keys($array) -> retorna um novo array com as chaves do array passado
+     * array_values($array) -> retorna um novo array com os valores do array pasado
+     * array_merge($array1, $array2) -> junta dois ou mais arrays
+     * array_pop() -> exclui a ultima posição do array
+     * array_shift() -> excluia primeira posição do array
      */
-    
-    /* array_keys */
     $nomes = array("primo" => "Rodrigo", "vizinho" => "Felipe", "mae" => "Maria", "pai" => "José");
-    $keys = array_keys($nomes);
-    print_r($keys);
+
+    /* array_shift */
+    // $carros = array("camaro", "uno", "gol");
+    // print_r($carros);
+    // echo "<br>";
+    // echo array_shift($carros);
+    // echo "<br>";
+    // print_r($carros);
+
+    /* array_pop */
+    // $carros = array("camaro", "uno", "gol");
+    // print_r($carros);
+    // echo "<br>";
+    // echo array_pop($carros);
+    // echo "<br>";
+    // print_r($carros);
+
+    /* array_merge */
+    // $carros = array("camaro", "uno", "gol");
+    // $motos = array("pop100", "500cc", "cb1000");
+    // $veiculos = array_merge($carros, $motos);
+    // print_r($veiculos);
+
+    /* array_values */
+    // $values = array_values($nomes);
+    // print_r($values);
+
+    /* array_keys */
+    // $keys = array_keys($nomes);
+    // print_r($keys);
 
      /* == in_array == */
     // if(in_array("Rodrigo", $nomes)){
